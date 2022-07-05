c88f8b1e3b329c7ae326ba64e4a05f51e912caf4
JoseMGuerra <Josem_guerra@hotmail.com>
2022-07-05 15:17:15 +0000

    Add variable with quiz questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,33 @@
-console.log( 'Im connected')
+const gameData = [
+    {
+      question:
+        "A small bunch of herbs, tied together, that is added to stews or casseroles for flavour.",
+      options: ["Bouquet garni", "Crudité", "Beignet", "Tuile"],
+      answer: 1,
+    },
+    {
+      question:
+        "French-style, bite-sized sweetmeats and cakes served with coffee.",
+      options: ["Coulis", "Crêpe", "Petits fours", "Canapé"],
+      answer: 3,
+    },
+    {
+      question:
+        "A French term describing the continual tossing of food in shallow fat so that it browns evenly.",
+      options: ["Pan frying", "Basting", "Sautéeing", "Braising"],
+      answer: 3,
+    },
+    {
+      question:
+        "Extracting the flavours of spices or herbs by soaking them in liquid heated in a covered pan.",
+      options: ["Blanching ", "Curdling", "Brine", "Infusing"],
+      answer: 4,
+    },
+    {
+      question:
+        "A French dish that has been sprinkled with sugar and grilled until a caramelized crust has formed on top.",
+      options: ["Julienne", "Brulée", "Caramel", "Flambé"],
+      answer: 2,
+    },
+  ];
+  
