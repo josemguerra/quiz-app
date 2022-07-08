a6405b7ac1b1448427e8c94dbf6465c14980a11b
JoseMGuerra <Josem_guerra@hotmail.com>
2022-07-08 17:51:33 +0000

    Fix validation errors and add comments in all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-/**
- * Database of question and answers for the quiz.
- */
+/*jshint esversion: 6 */
+
+// Variable containing the quiz questions 
+
 const gameData = [{
     question: "A small bunch of herbs, tied together, that is added to stews or casseroles for flavour.",
     answers: ["Bouquet garni", "Crudité", "Beignet", "Tuile"],
@@ -52,15 +53,20 @@ const gameData = [{
     correct: 1,
   },
 ];
+
+// Variables of Selected elements
+
 const bannerText = document.querySelector("#banner");
 const userNameContainer = document.querySelector("#user-name-container");
+const welcomeText = document.querySelector("#welcomeText");
+const feedbackText = document.querySelector("#feedback-text");
 const userName = document.querySelector("#username");
 const startButton = document.querySelector("#start-button");
 const nextButton = document.querySelector("#next-button");
 const tryAgainButton = document.querySelector("#reload-button");
 const quizContainer = document.querySelector("#quiz-container");
 const questionText = document.querySelector("#question");
-const alert = document.querySelector("#alert");
+const modalMessage = document.querySelector("#modal-message");
 const answersContainer = document.querySelector("#answers-container");
 const scoreCard = document.querySelector("#score-card");
 const answer1 = document.querySelector("#answer1");
@@ -70,21 +76,35 @@ const answer4 = document.querySelector("#answer4");
 
 let scoreIndex = 0;
 let questionIndex = 0;
-/**
- * Event Listeners
- */
-document.addEventListener("DOMContentLoaded", () => {});
+
+
+//  // Wait for the DOM to finish loading before running the game.
+
+document.addEventListener("DOMContentLoaded", () => {
+  feedbackText.textContent = '';
+
+});
+//  Event listeners control butttons
 
 startButton.addEventListener("click", e => {
   e.preventDefault(), validateUser();
 });
+userName.addEventListener("keydown", e => {
+  if (e.key === "Enter") {
+    validateUser();
+  }
+});
 nextButton.addEventListener("click", () => {
   next();
 });
+
 tryAgainButton.addEventListener("click", () => {
   location.reload();
 });
 
+/**
+ * Validates that username input is not left empty.
+ */
 function validateUser() {
   const userVal = document.querySelector("#username");
   if (userVal.value == "") {
@@ -95,20 +115,25 @@ function validateUser() {
     startGame();
   }
 }
-
+/**
+ * Activate answer buttons individually.
+ */
 function activateButtons() {
   answersContainer.querySelectorAll(".answer-button").forEach((button, key) => {
     button.addEventListener("click", selectedAnswer);
     button.answerId = key + 1;
   });
 }
-
+/**
+ * Deactivate answer buttons to prevent answering multiple times the same question.
+ */
 function deactivateButtons() {
   answersContainer.querySelectorAll(".answer-button").forEach((button, key) => {
     button.removeEventListener("click", selectedAnswer);
     button.answerId = key + 1;
   });
 }
+// This functions hide and display DOM elements for user interactivity
 
 function hideQuiz() {
   quizContainer.style.display = "none";
@@ -118,15 +143,24 @@ hideQuiz();
 function displayQuiz() {
   quizContainer.style.display = "flex";
 }
-
+/**
+ * This hides Next button in between question to avoid skipping question without answering them.
+ */
 function hideNexButton() {
   nextButton.style.display = "none";
 }
-
+/**
+ * Next button is displayed so the user can move into the next question of the quiz.
+ */
 function displayNexButton() {
   nextButton.style.display = "flex";
 }
-
+/**
+ * This will hide Username Input and Start button, and display the Quiz questions as well as answer buttons  and Score area.
+ * It will display a welcome message with the choosen username.
+ * It will hide quiz banner text.
+ * It will hide try again button to avoid page reload without answering all the questions.
+ */
 function startGame() {
   activateButtons();
   hideNexButton();
@@ -136,7 +170,10 @@ function startGame() {
   tryAgainButton.style.display = "none";
   displayQuiz();
 }
-
+/**
+ * This will initiate the quiz iterating through questions and answers.
+ * 
+ */
 function startQuiz() {
   const currentGameData = gameData[questionIndex];
   if (questionIndex < gameData.length) {
@@ -151,12 +188,15 @@ function startQuiz() {
     answersContainer.style.display = "none";
     questionText.innerText = "Quiz Completed!";
     welcomeText.textContent = "";
-
     hideNexButton();
     tryAgainButton.style.display = "flex";
   }
 }
-
+/**
+ * Check that the button pressed correcponde with the correct answer.
+ * If it is correct,incrememts score by one and changes background color to green.
+ * Otherwise will change background color to red .
+ */
 function selectedAnswer(e) {
   let currentQuestion = gameData[questionIndex];
   let currentButton = e.currentTarget.answerId;
@@ -172,6 +212,7 @@ function selectedAnswer(e) {
   displayNexButton();
   deactivateButtons();
 }
+// Next question
 
 function next() {
   questionIndex++;
@@ -181,28 +222,32 @@ function next() {
   startQuiz();
   fullScore();
 }
-
+/**
+ * Calcutale Score by comparing number questions that had been answered correctly against the total amount.
+ */
 function currentScore() {
   scoreCard.textContent = `${scoreIndex} / ${gameData.length}`;
 }
+// Displays a thank you message using the user name.
 
 function thankYouMessage() {
-  thankYouText.textContent = `Thank you for taking my Quiz ${userName.value}!`;
+  feedbackText.textContent = `Thank you for taking my Quiz ${userName.value}!`;
 }
+
 /**
- * This is a modal that will display if the user get all questions right
+ * This will display a Modal  message if the user answers all questions and them all are right.
  */
 function fullScore() {
   if (scoreIndex === gameData.length) {
-    alert.classList.add("active");
+    modalMessage.classList.add("active");
     hideQuiz();
     setTimeout(() => {
-      alert.classList.remove("active");
-    }, 2500);
+      modalMessage.classList.remove("active");
+    }, 3300);
     setTimeout(() => {
       displayQuiz();
       thankYouMessage();
-    }, 2700);
+    }, 3300);
   }
 }
 
